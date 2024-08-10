dab7e07568499990a25202553c6ad81a09670b9c
AhmedSoliman00 <himozhima@gmail.com>
2024-08-10 13:14:20 +0300

    second client

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3001,
     host: "127.0.0.1",
   },
   plugins: [react()],
